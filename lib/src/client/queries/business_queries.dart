82fe19d29a3f42f488a82ed42aedea4a33c2e53c
yendu <yendu669@gmail.com>
2021-04-13 08:09:36 +0300

    business queries

--- lib/src/client/queries/business_queries.dart
+++ lib/src/client/queries/business_queries.dart
@@ -58,3 +58,76 @@ query ($id: MongoID!){
   }
 }
 """;
+const GET_BUSINESS_MANY=r"""
+  query($limit:Int,$filter:FilterFindManyBusinessInput){
+     businessMany(limit:$limit,filter:$filter){
+    _id
+    businessName
+    phoneNumber
+    location
+    emails
+    website
+    logoPics
+    slogan
+    description
+    specialization
+    categoriesName
+    history
+    establishedIn
+    subscription
+    updatedAt
+    createdAt
+    pictures
+    owner {
+      fullName
+      email
+      phoneNumber
+      firebaseId
+      businesses {
+        _id
+        businessName
+      }
+    }
+    openHours {
+      day
+      opens
+      closes
+    }
+    posts {
+      _id
+      description
+      videos
+      photos
+      createdAt
+    }
+    events {
+      _id
+      title
+      description
+      location
+      link
+      videos
+      photos
+      createdAt
+    }
+    categories {
+      _id
+      name
+      parent
+    }
+     
+     }
+  
+  
+  }
+
+""";
+
+const  GET_BUSINESS_LIST_MANY=r"""
+  query{
+   businessListMany{
+    _id,
+    autoCompleteTerm
+   }
+  }
+  """;
